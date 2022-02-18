50ca60f43061c8a860d7ceb7f741e087eb7e3e73
csw609 <csw609@naver.com>
2022-02-18 14:44:01 +0900

    update

--- src/plane_seg.cpp
+++ src/plane_seg.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include <std_msgs/String.h>
 
+#include <iostream>
+
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
@@ -12,6 +14,10 @@
 
 std::queue<sensor_msgs::ImageConstPtr> rgb_image_buf;
 std::queue<sensor_msgs::ImageConstPtr> depth_image_buf;
+
+float fx = 384.58409758650083;
+float cx = 324.23845707433316;
+float cy = 235.9118314160069;
 
 void rgb_sub(const sensor_msgs::ImageConstPtr &rgb_image)
 {
@@ -27,21 +33,25 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "door");
   ros::NodeHandle nh;
-  ros::Subscriber rgb_image_sub = nh.subscribe("camera/infra1/image_rect_raw", 1000, rgb_sub); //config file parameter
-  ros::Subscriber depth_image_sub = nh.subscribe("camera/infra2/image_rect_raw", 1000, depth_sub);
+  ros::Subscriber rgb_image_sub   = nh.subscribe("/camera/color/image_raw", 1000, rgb_sub);
+  ros::Subscriber depth_image_sub = nh.subscribe("/camera/aligned_depth_to_color/image_raw", 1000, depth_sub);
 
-  ros::Publisher img_match_pub = nh.advertise<sensor_msgs::Image>("match_image", 1000);
+  ros::Publisher img_match_pub  = nh.advertise<sensor_msgs::Image>("match_image", 1000);
+  ros::Publisher pointcloud_pub = nh.advertise<sensor_msgs::PointCloud2>("door", 1000);
 
   sensor_msgs::Image match_image;
   cv_bridge::CvImage cv_bridge;
   std_msgs::Header header;
 
-  ros::Rate loop_rate(50);
+  ros::Rate loop_rate(15);
+  bool first = true;
+  sensor_msgs::PointCloud2 cloudmsg;
 
   while (ros::ok())
   {
     if (!rgb_image_buf.empty() && !depth_image_buf.empty())
     {
+      //ROS_INFO("buf no empty");
       double time;
       double time_l = rgb_image_buf.front()->header.stamp.toSec();
       double time_r = depth_image_buf.front()->header.stamp.toSec();
@@ -65,9 +75,9 @@ int main(int argc, char **argv)
 
         //sensor_msgs::image to cv::Mat
         cv_bridge::CvImageConstPtr ptr_l;
-        ptr_l = cv_bridge::toCvCopy(rgb_image_buf.front(), sensor_msgs::image_encodings::MONO8);
+        ptr_l = cv_bridge::toCvCopy(rgb_image_buf.front(), sensor_msgs::image_encodings::BGR8);
         cv_bridge::CvImageConstPtr ptr_r;
-        ptr_r = cv_bridge::toCvCopy(depth_image_buf.front(), sensor_msgs::image_encodings::MONO8);
+        ptr_r = cv_bridge::toCvCopy(depth_image_buf.front(), sensor_msgs::image_encodings::TYPE_16UC1);
 
         rgb_image = ptr_l->image.clone();
         rgb_image_buf.pop();
@@ -75,18 +85,47 @@ int main(int argc, char **argv)
         depth_image_buf.pop();
       }
 
-      if (!rgb_image.empty() && !depth_image.empty())
+      if (first && !rgb_image.empty() && !depth_image.empty())
       {
+        int c1 = 213;
+        int r1 = 160;
+        int c2 = 426;
+        int r2 = 320;
+        //std::cout << depth_image.at<unsigned short>(r1,c1) << std::endl;
+        //std::cout << depth_image.size() << std::endl;
         pcl::PointCloud<pcl::PointXYZRGB> cloud;
+        pcl::PointXYZRGB point;
+        for(int i = r1; i < r2; i++){
+          for(int j = c1; j < c2; j++){
+            point.z = depth_image.at<unsigned short>(i,j) / 1000.0;
+            point.x = (static_cast<float>(j)-cx) * point.z  / fx;
+            point.y = (static_cast<float>(i)-cy) * point.z / fx;
+            point.b = rgb_image.at<cv::Vec3b>(i,j)[0];
+            point.g = rgb_image.at<cv::Vec3b>(i,j)[1];
+            point.r = rgb_image.at<cv::Vec3b>(i,j)[2];
+            cloud.push_back(point);
+
+            //std::cout << point.x << "  " << point.y << "  " << point.z << std::endl;
 
 
+          }
+        }
 
+        pcl::toROSMsg(cloud, cloudmsg);
+        cloudmsg.header.frame_id = "camera";
 
+        //first = false;
+        //ROS_INFO("Make Cloud");
       }
-
-      ros::spinOnce();
+    }
+    else
+    {
+      ROS_INFO("Wait Images");
     }
 
+    pointcloud_pub.publish(cloudmsg);
+    ros::spinOnce();
+    loop_rate.sleep();
   }
 
 
